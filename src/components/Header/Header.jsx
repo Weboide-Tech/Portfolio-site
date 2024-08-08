4a5371c94c5a0582a06369774aaf56ca96219df8
Rehan Akhtar <61869629+rehan985@users.noreply.github.com>
2024-08-08 13:17:52 +0500

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
     <header>
       <div className="container header_container">
 <h5>Hello I'm</h5>
-<h1>Muhammad</h1>
+<h1>Workshop</h1>
 
 <h5 className="text-light">
 <Typewriter
